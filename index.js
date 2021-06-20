71cbf8836498e6380a202a73c5ea9cf38d3c61ec
YashrajV252 <84023297+YashrajV252@users.noreply.github.com>
2021-06-20 18:04:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function big() {
+    var a = document.getElementById ("info")
+    a.style.fontSize = "100px"
+}
+
+function normal() {
+var b = document.getElementById ("info")
+b.style.fontSize = "40px"
+}
+
+function small() {
+    var b = document.getElementById ("info")
+    b.style.fontSize = "20px"
+    }
